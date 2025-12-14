b8c945f4b5659612569f9c2a4a2d58ed1aa656a0
xxc <tasmira@qq.com>
2025-12-14 22:09:23 +0800

    打印日志类型更新

--- src/receiver.rs
+++ src/receiver.rs
@@ -8,21 +8,22 @@ use std::{
 pub fn handle_receive(addr: impl ToSocketAddrs, output_path: &str) {
     let output_path = PathBuf::from(output_path);
     create_dir_all(&output_path).unwrap();
-    tracing::debug!("保存文件的目录：{}", output_path.display());
+    tracing::info!("保存文件的目录：{}", output_path.display());
 
     let stream = TcpListener::bind(addr).unwrap();
-    tracing::debug!("服务器启动，监听{}", stream.local_addr().unwrap());
+    tracing::info!("服务器启动，监听{}", stream.local_addr().unwrap());
 
     loop {
         let output_path = output_path.clone();
 
         let (stream, a) = stream.accept().unwrap();
-        tracing::debug!("新连接：{}", a);
+        tracing::info!("新连接：{}", a);
 
+        tracing::info!("接收文件中...");
         std::thread::spawn(move || {
             let mut stream = ReceiveProtocol::new(stream);
             stream.receive_file_or_dir(&output_path);
-            tracing::debug!("接收任务完成");
+            tracing::info!("接收任务完成");
         });
     }
 }
--- src/sender.rs
+++ src/sender.rs
@@ -7,14 +7,13 @@ use std::{
 use walkdir::WalkDir;
 
 pub fn handle_send(addr: impl ToSocketAddrs, send_path: &str) {
-    let start_time = Instant::now();
-
     let send_path = PathBuf::from(send_path);
     if !send_path.exists() {
         panic!("需要发送的路径{}不存在", send_path.display());
     }
 
     let stream = TcpStream::connect(addr).unwrap();
+    tracing::info!("连接到服务器成功");
 
     let mut stream = SendProtocol::new(stream);
 
@@ -22,12 +21,15 @@ pub fn handle_send(addr: impl ToSocketAddrs, send_path: &str) {
 
     let paths = WalkDir::new(&send_path);
 
+    tracing::info!("发送文件中...");
+    let start_time = Instant::now();
     for entry in paths {
         let entry = entry.unwrap();
         let path = entry.path();
 
         stream.send_file_or_dir(path, root_dir);
     }
+
     stream.flush();
-    tracing::debug!("发送任务完成，耗时: {:?}", start_time.elapsed());
+    tracing::info!("发送任务完成，耗时: {:?}", start_time.elapsed());
 }
